09d2563a0d73f430d998c7f7c9d82a1932bd9021
NeoCrtxBoil <148251927+NeoCrtxBoil@users.noreply.github.com>
2023-10-17 18:59:45 +0300

    Update Eiler_6.py

--- Eiler_6.py
+++ Eiler_6.py
@@ -1,10 +1,10 @@
 # https://projecteuler.net/problem=6
 
 def Eiler_6(m :int):
-	total = (m * (1 + m) // 2) ** 2
+	result = (m * (1 + m) // 2) ** 2
 	for c in range(1, m + 1):
-		total -= c ** 2
-	return total
+		result -= c ** 2
+	return result
 
 if __name__ == "__main__":
 	print(Eiler_6(100))
