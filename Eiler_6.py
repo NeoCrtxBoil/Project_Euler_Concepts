302fe967c197c89b7e30a93d0f1f948bb4ba1a53
NeoCrtxBoil <148251927+NeoCrtxBoil@users.noreply.github.com>
2024-02-19 13:29:54 +0300

    Update Eiler_6.py

--- Eiler_6.py
+++ Eiler_6.py
@@ -1,8 +1,9 @@
 # https://projecteuler.net/problem=6
 
 def Eiler_6(m :int):
-	result = (m * (1 + m) // 2) ** 2
-	result -= m * (m + 1) * (2 * m + 1) // 6
+#	result = (m * (1 + m) // 2) ** 2		# (1 + 2 + ... + m)^2		squared sum of 1-st m terms of arithmetic progression (a1 = 1 ; d = 1)
+#	result -= m * (m + 1) * (2 * m + 1) // 6	# (1^2 + 2^2 + ... + m^2)	Formula of the sum of squares of the first m numbers
+	result = int(m * (m * m - 1) * (m + 2 / 3) // 4)
 	return result
 
 if __name__ == "__main__":
